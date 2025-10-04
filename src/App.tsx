e3cd2ac177e10113ff991a7c4a32b9b9e28bc3f7
andrehmf23 <andrehmf23@gmail.com>
2025-10-04 14:53:18 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,37 @@ import { ContextProviders } from "./context";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Dashboard</Link> | <Link to="/contracts">Contracts</Link>
+      <nav className="bg-gray-800 p-4 flex gap-6">
+        <Link
+          to="/"
+          className="text-white font-semibold hover:text-yellow-400 transition-colors"
+        >
+          Dashboard
+        </Link>
+        <Link
+          to="/contracts"
+          className="text-white font-semibold hover:text-yellow-400 transition-colors"
+        >
+          Contracts
+        </Link>
+        <Link
+          to="/about"
+          className="text-white font-semibold hover:text-yellow-400 transition-colors"
+        >
+          About
+        </Link>
+        <Link
+          to="/orbit"
+          className="text-white font-semibold hover:text-yellow-400 transition-colors"
+        >
+          Orbit
+        </Link>
+        <Link
+          to="/rocket"
+          className="text-white font-semibold hover:text-yellow-400 transition-colors"
+        >
+          Rocket
+        </Link>
       </nav>
 
       <Routes>
