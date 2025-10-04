59bdf7fa9bcca411255e1d5db9e0a7d278a9299f
andrehmf23 <andrehmf23@gmail.com>
2025-10-04 11:53:17 -0300

    f

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />
+);
+
+export default App;
