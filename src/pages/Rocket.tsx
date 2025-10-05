d0ca940cde7489112971ac5df4a2368b2673da30
andrehmf23 <andrehmf23@gmail.com>
2025-10-05 09:58:24 -0300

    Add fuel inputs

--- src/pages/Rocket.tsx
+++ src/pages/Rocket.tsx
@@ -13,7 +13,7 @@ const Rocket: React.FC = () => {
     country: ""
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     setForm({ 
       ...form, 
       [e.target.name]: e.target.value 
@@ -22,67 +22,94 @@ const Rocket: React.FC = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    if (!form.name || !form.height || !form.weight || !form.fuel || !form.country) {
+      setError("Por favor, preencha todos os campos.");
+      return;
+    }
+
     addSatellite(form);
     setForm({ name: "", height: "", weight: "", fuel: "", country: "" });
   };
 
   return <>
-      <div className="flex flex-col border-2 bg-[#ffffff05] border-white p-4 rounded-md max-w-md mx-auto mt-20">
-        <h1 className="text-2xl font-bold mb-4">Formulário do Foguete</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-          <input
-            type="text"
-            name="name"
-            placeholder="Nome do foguete"
-            value={form.name}
-            onChange={handleChange}
-            className="border p-2 rounded"
-            required
-          />
-          <input
-            type="number"
-            name="height"
-            placeholder="Altura (m)"
-            value={form.height}
-            onChange={handleChange}
-            className="border p-2 rounded"
-            required
-          />
-          <input
-            type="number"
-            name="weight"
-            placeholder="Peso (kg)"
-            value={form.weight}
-            onChange={handleChange}
-            className="border p-2 rounded"
-            required
-          />
-          <input
-            type="text"
-            name="fuel"
-            placeholder="Combustível"
-            value={form.fuel}
-            onChange={handleChange}
-            className="border p-2 rounded"
-            required
-          />
-          <input
-            type="text"
-            name="country"
-            placeholder="País de origem"
-            value={form.country}
-            onChange={handleChange}
-            className="border p-2 rounded"
-            required
-          />
-          <button
-            type="submit"
-            className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 "
-          >
-            Enviar
-          </button>
-        </form>
-      </div>
+    <div className="flex flex-col border-2 border-white p-4 rounded-md max-w-md mx-auto mt-20">
+      {error && <p className="text-red-500 font-semibold mt-2">{error}</p>}
+      <h1 className="text-2xl font-bold mb-4">Formulário do Foguete</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome do foguete"
+          value={form.name}
+          onChange={handleChange}
+          className="border p-2 rounded"
+          required
+        />
+        <input
+          type="number"
+          name="height"
+          placeholder="Altura (m)"
+          value={form.height}
+          onChange={handleChange}
+          className="border p-2 rounded"
+          required
+        />
+        <input
+          type="number"
+          name="weight"
+          placeholder="Peso (kg)"
+          value={form.weight}
+          onChange={handleChange}
+          className="border p-2 rounded"
+          required
+        />
+        <input
+          type="text"
+          name="Fuel mass"
+          placeholder="Fuel mass (kg)"
+          value={form.fuel}
+          onChange={handleChange}
+          className="border p-2 rounded"
+          required
+        />
+        <input
+          type="text"
+          name="fuel"
+          placeholder="Combustível"
+          value={form.fuel}
+          onChange={handleChange}
+          className="border p-2 rounded"
+          required
+        />
+        <select
+          name="type fuel"
+          value={form.fuel}
+          onChange={handleChange}
+          className="border p-2 rounded bg-neutral-800 "
+          required
+        >
+          <option value="">Select fuel type</option>
+          <option value="Kerosene">RP-1 + LOX (kerosene)</option>
+          <option value="Liquid hydrogen + LOX">Liquid hydrogen + LOX</option>
+          <option value="Solid">Solid (military rocket/jet)</option>
+        </select>
+        <input
+          type="text"
+          name="country"
+          placeholder="País de origem"
+          value={form.country}
+          onChange={handleChange}
+          className="border p-2 rounded"
+          required
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 "
+        >
+          Enviar
+        </button>
+      </form>
+    </div>
   </>;
 };
 
